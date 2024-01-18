fb49b0743d3480ab56032e84504fbaee0a93068e
Mbang Ernest N <mbangernest@yahoo.com>
2024-01-18 14:31:11 +0100

    sql script to create basic users table

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- this script creates a table with parameters
+--  id, email and name with their unique contrains
+CREATE TABLE users IF NOT EXISTS (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);

2a00823d1914b1dd9564ed632d93858938c9a353
Mbang Ernest N <mbangernest@yahoo.com>
2024-01-19 14:41:26 +0100

    create table with enumeration and default contraint

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- This script creates a table in a database
+-- users: id, email, name, country
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country VARCHAR(2) NOT NULL DEFAULT 'US',
+    CHECK (country IN ('US', 'CO', 'TN'))
+);
